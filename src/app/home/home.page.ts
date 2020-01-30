531af0540c21c82228fc5b827ac9bd39fa9fc6b9
joelkingsleyr <jkingsley@nvenergy.com>
2020-01-30 22:54:17 +0530

    Added basic code for creating the audio player, added 3 sermons as sample data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Howl } from 'howler';
+import { IonRange } from '@ionic/angular';
+
+export interface Track {
+  name: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +13,86 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  playlist: Track[] = [
+    {
+      name: 'Standing in the Gap',
+      path: './assets/mp3/060312p.mp3'
+    },
+    {
+      name: 'Once Saved Always Saved',
+      path: './assets/mp3/022314a.mp3'
+    },
+    {
+      name: 'Why We Need Hard Preaching?',
+      path: './assets/mp3/041314a.mp3'
+    }
+  ];
+
+  activeTrack: Track;
+  player: Howl;
+  isPlaying: boolean = false;
+  progress: number = 0;
+  @ViewChild('range', { static: false }) range: IonRange;
 
   constructor() {}
 
+  start(track: Track) {
+    if (this.player) {
+      this.player.stop();
+    }
+    this.player = new Howl({
+      src: [track.path],
+      html5: true,
+      onplay: () => {
+        console.log('onplay');
+        this.isPlaying = true;
+        this.activeTrack = track;
+      },
+      onend: () => {
+
+      }
+    })
+    this.player.play();
+  }
+
+  togglePlayer(pause: boolean) {
+    this.isPlaying = !pause;
+    if (pause) {
+      this.player.pause();
+    } else {
+      this.player.play();
+    }
+  }
+
+  next() {
+    let index = this.playlist.indexOf(this.activeTrack);
+    if (index != this.playlist.length - 1) {
+      this.start(this.playlist[index + 1]);
+    } else {
+      this.start(this.playlist[0]);
+    }
+  }
+
+  prev() {
+    let index = this.playlist.indexOf(this.activeTrack);
+    if (index > 0) {
+      this.start(this.playlist[index - 1]);
+    } else {
+      this.start(this.playlist[this.playlist.length - 1]);
+    }
+  }
+
+  seek() {
+    let newValue = +this.range.value;
+    let duration = this.player.duration();
+    this.player.seek(duration * (newValue / 100));
+  }
+
+  updateProgress() {
+    let seek = this.player.seek();
+    this.progress = (seek / this.player.duration()) * 100 || 0;
+    setTimeout(() => {
+      this.updateProgress();
+    }, 1000);
+  }
 }
