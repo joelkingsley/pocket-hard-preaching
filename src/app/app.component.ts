2d0e86bbab1179c85280299679212e969adee196
joelkingsleyr <jkingsley@nvenergy.com>
2020-02-03 17:52:09 +0530

    Added code to check for updates and download them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Deploy } from 'plugins/cordova-plugin-ionic/dist/ngx';
+import { CheckForUpdateResponse } from 'plugins/cordova-plugin-ionic/dist/IonicCordova';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private deploy: Deploy
   ) {
     this.initializeApp();
   }
@@ -23,5 +26,26 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+
+    if (this.platform.is('cordova')) {
+      let config = {
+        appId: '6af104c9',
+        channel: 'Master'
+      };
+
+      this.deploy.configure(config);
+
+      this.deploy.checkForUpdate().then(async (update: CheckForUpdateResponse) => {
+        if (update.available){
+          await this.deploy.downloadUpdate((progress) => {
+            console.log('download', progress);
+          })
+          await this.deploy.extractUpdate((progress) => {
+            console.log(progress);
+          })
+          await this.deploy.reloadApp();
+        }
+      });
+    }
   }
 }
